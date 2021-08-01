db26ac343defa676b9744d2682e3d4f681308b3b
Roshan Melvin <74945888+roshan648@users.noreply.github.com>
2021-08-01 14:49:05 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,10 +1,44 @@
 # ydl1.py
 from __future__ import unicode_literals
 import youtube_dl
-link = input("Enter your link:")
+action=input('Ender your option a/v:')
+if action=='a':
+ link = input("Enter your link:")
 
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+ ydl_opts = {}
+ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([link])
+elif action=='v':
+    # ydl1.py
+ from __future__ import unicode_literals
+ import youtube_dl
+ class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+ def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+ }
+ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
     
  
