7aeedb19d2e285168e897970d4ce617c274fc6f4
TYech5 <74945888+roshan648@users.noreply.github.com>
2022-03-04 15:58:54 +0530

    Update python3.sh

--- Linux/python3.sh
+++ Linux/python3.sh
@@ -1,10 +1,10 @@
 clear
 cd ..
-sudo -i
-apt update 
 
-pip3 install python-cfonts
-pip3 install  youtube_dl  
-apt install ffmpeg
-pip3 install ffmpeg
-python3 newfile.py 
+sudo apt update 
+
+sudo pip3 install python-cfonts
+sudo pip3 install  youtube_dl  
+sudo apt install ffmpeg
+sudo pip3 install ffmpeg
+sudo python3 newfile.py 
